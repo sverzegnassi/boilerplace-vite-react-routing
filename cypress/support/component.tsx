68ad4d37510ad31ea7bb8fa1f663a702fa1c02b6
Stefano Verzegnassi <stefano@sverzegnassi.me>
2023-07-25 09:32:55 +0200

    Fixed build errors

--- cypress/support/component.tsx
+++ cypress/support/component.tsx
@@ -1,12 +1,9 @@
 import './commands'
 import { mount } from 'cypress/react18'
-import React from 'react'
 import { MemoryRouter } from 'react-router-dom'
 
 Cypress.Commands.add('mount', (component, options = {}) => {
-  const { routerProps = { initialEntries: ['/'] }, ...mountOptions } = options
+  const wrapped = <MemoryRouter initialEntries={['/']}>{component}</MemoryRouter>
 
-  const wrapped = <MemoryRouter {...routerProps}>{component}</MemoryRouter>
-
-  return mount(wrapped, mountOptions)
+  return mount(wrapped, options)
 })
